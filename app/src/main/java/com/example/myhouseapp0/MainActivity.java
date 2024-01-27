2bc22dc068dd5038f991aee5d93630dc0a60415b
anastasiaplat <anastasiaplat@inbox.ru>
2024-01-27 13:33:47 +0300

    tried to make transition to register but with clicking the app is closing

--- app/src/main/java/com/example/myhouseapp0/MainActivity.java
+++ app/src/main/java/com/example/myhouseapp0/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.myhouseapp0;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_sign_in, btn_registration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_sign_in = findViewById(R.id.btn_sign_in);
+        btn_registration = findViewById(R.id.btn_registration);
+
+        btn_registration.setOnClickListener(v -> {
+            Intent intent = new Intent(".RegistrationActivity");
+            startActivity(intent);
+        });
+
+
+        
     }
+
 }
