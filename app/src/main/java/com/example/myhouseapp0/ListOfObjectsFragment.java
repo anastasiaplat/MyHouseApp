e7526089527b588ef7582fd5add1892b04ee1c4c
anastasiaplat <anastasiaplat@inbox.ru>
2026-03-22 15:59:46 +0300

    added one more class and UI for main activity

--- app/src/main/java/com/example/myhouseapp0/ListOfObjectsFragment.java
+++ app/src/main/java/com/example/myhouseapp0/ListOfObjectsFragment.java
@@ -8,12 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class InteractiveMapFragment extends Fragment {
 
-        @Override
+public class ListOfObjectsFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_interactive_map, container, false);
+        return inflater.inflate(R.layout.fragment_list_of_objects, container, false);
     }
 }
