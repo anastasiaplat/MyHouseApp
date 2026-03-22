8bfac1207f0423f7596c021edbf744003bf098a0
anastasiaplat <anastasiaplat@inbox.ru>
2026-03-22 14:53:10 +0300

    added class and layout for  new fragment

--- app/src/main/java/com/example/myhouseapp0/HomeFragment.java
+++ app/src/main/java/com/example/myhouseapp0/HomeFragment.java
@@ -16,11 +16,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.ViewFlipper;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.myhouseapp0.rooms.BathroomFragment;
